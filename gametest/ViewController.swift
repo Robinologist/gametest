f1c86a4c5fee39e2e61198f6f2a2e9180ce01772
Upgradr <Robinologist@gmail.com>
2023-05-15 14:27:38 -0400

    Xcode are you drunk?

--- gametest/ViewController.swift
+++ gametest/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet var image: UIImageView!
+    
+    @IBAction func button(_ sender: Any) {
+        print("Velo+=18")
+    }
+    
+    var Velocity = 0
+    print(Velocity)
+    
 }
-
